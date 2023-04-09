934efe675874cfa11e5a23d52c608b90fdac263b
m0rl0ck <seriy17@yandex.ru>
2023-04-09 11:10:38 +0300

    fix: fix lint warning

--- src/pages/main/mainPage.test.tsx
+++ src/pages/main/mainPage.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import connector from '../../utils/connector/Connector';
-import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import MainPage from './MainPage';
 import ICharacterCard from '../../infostructure/ICharacterCard';
